fe5b2231ab77f25f0bc7a90915e631133174e672
Daniel Peterson <danrpts@gmail.com>
2016-03-06 04:42:30 -0800

    touch ups

--- haskell/escape_time_ascii_varied.hs
+++ haskell/escape_time_ascii_varied.hs
@@ -1,20 +1,20 @@
 import Data.Complex
  
 reals :: [Double]
-reals = [-2,-1.9685..0.5]
+reals = [-2,-1.97..0.5]
 
 imags :: [Double]
 imags = [1,0.95..(-1)]
 
--- extract escape-time iteration
+-- extract escape iteration
 mandelbrot :: Int -> Complex Double -> Int
-mandelbrot n c = length $ filter (\z -> magnitude z > 2) 
-                $ take n $ iterate (\z -> z * z + c) 0
+mandelbrot n c = length $ filter  (\z -> magnitude z > 2) 
+               $ take n $ iterate (\z -> z * z + c) 0
 
 -- select ascii char
 escape :: Int -> Complex Double -> Char
 escape n c = toEnum $ (+33) $ mandelbrot n c
 
 main :: IO ()
-main = mapM_ putStrLn [ [ escape 15 (r :+ i) | r <- reals] | i <- imags ]
+main = mapM_ putStrLn [ [ escape 15 (r :+ i) | r <- reals ] | i <- imags ]
 -- notice 15 ascii symbols [33..47]
