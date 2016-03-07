a46172307e00de2d78e3b13b8b95e04018e369c4
Daniel Peterson <danrpts@gmail.com>
2016-03-07 00:50:01 -0800

    comments and code clean up

--- haskell/escape_time_ascii_basic.hs
+++ haskell/escape_time_ascii_basic.hs
@@ -1,26 +1,26 @@
 import Data.Complex
  
 depth :: Int
-depth = 11 -- arbitrary max iteration
+depth = 11 -- max iteration
 
 bailout :: Double
 bailout = 2.0 -- escape radius
 
 reals :: [Double]
-reals = [-2,-1.975..0.5]
+reals = [-2,-1.975..0.5] -- real axis
 
 imags :: [Double]
-imags = [1,0.95..(-1)]
+imags = [1,0.95  ..(-1)] -- imaginary axis
 
 -- extract escape magnitude
 mandelbrot :: Complex Double -> Double
 mandelbrot c = magnitude $ iterate (\z -> z * z + c) 0 !! depth
 
--- escaped or not
-escape :: Complex Double -> Char
-escape c
+-- magnitude descirbes if it escape
+describe :: Complex Double -> Char
+describe c
    | mandelbrot c > bailout = ' '
    | otherwise              = '*'
 
 main :: IO ()
-main = mapM_ putStrLn [ [ escape (r :+ i) | r <- reals ] | i <- imags ]
+main = mapM_ putStrLn [ [ describe $ r :+ i | r <- reals ] | i <- imags ]
--- haskell/escape_time_ascii_varied.hs
+++ haskell/escape_time_ascii_varied.hs
@@ -1,25 +1,26 @@
 import Data.Complex
 
 depth :: Int
-depth = 15 -- 15 ascii symbols [33..47]
+depth = 15 -- max iteration
 
 bailout :: Double
 bailout = 2.0 -- escape radius
 
 reals :: [Double]
-reals = [-2,-1.975..0.5]
+reals = [-2,-1.975..0.5] -- real axis
 
 imags :: [Double]
-imags = [1,0.95..(-1)]
+imags = [1,0.95  ..(-1)] -- imaginary axis
 
 -- extract escape iteration
 mandelbrot :: Complex Double -> Int
 mandelbrot c = length $ filter (\z -> magnitude z > bailout) 
              $ take depth $ iterate (\z -> z * z + c) 0
+             -- depth is 15 for ascii chars [33..47]
 
--- select ascii char
-escape :: Complex Double -> Char
-escape c = toEnum $ (+33) $ mandelbrot c
+-- iteration describes when it escaped
+describe :: Complex Double -> Char
+describe c = toEnum $ (+33) $ mandelbrot c
 
 main :: IO ()
-main = mapM_ putStrLn [ [ escape (r :+ i) | r <- reals ] | i <- imags ]
+main = mapM_ putStrLn [ [ describe $ r :+ i | r <- reals ] | i <- imags ]
