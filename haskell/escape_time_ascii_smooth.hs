36c6f22ad4b919fb97b37988e3a83d0da22979d5
Daniel Peterson <danrpts@gmail.com>
2016-03-07 23:36:00 -0800

    version 3

---
+++ haskell/escape_time_ascii_smooth.hs
@@ -0,0 +1,30 @@
+import Data.Complex
+ 
+depth :: Int
+depth = 10 -- max iteration
+
+bailout :: Double
+bailout = 2.0 -- escape radius
+
+reals :: [Double]
+reals = [-2,-1.975..0.5] -- real axis
+
+imags :: [Double]
+imags = [1,0.95  ..(-1)] -- imaginary axis
+
+-- extract escape magnitude & iteration
+mandelbrot :: Complex Double -> (Int, Double)
+mandelbrot c  = (l, magnitude $ zs !! l) 
+   where   l  = length zs
+           zs = filter (\z -> magnitude z > bailout) 
+              $ take depth $ iterate (\z -> z * z + c) 0
+              -- depth is 10 for ascii chars [48..57]
+
+-- iteration describes when it escaped
+describe :: Complex Double -> Char
+describe c = toEnum $ (+48) $ i
+    where (i, m) = mandelbrot c
+    -- i & m together can be used to smooth out banding
+
+main :: IO ()
+main = mapM_ putStrLn [ [ describe $ r :+ i | r <- reals ] | i <- imags ]
